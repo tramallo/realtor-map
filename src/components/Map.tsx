dd61188da85fc06b312918fbc717bf0565e769cf
tramallo <timo.22394@hotmail.com>
2024-10-18 18:35:07 -0300

    add leaflet types || App -> remove template style | add simple style | remove template content & add Map component || delete index.css style || main.tsx -> remove inde.css import || new component Map || new component MapEventHandler

---
+++ src/components/Map.tsx
@@ -0,0 +1,38 @@
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { LatLng, LeafletEventHandlerFnMap } from 'leaflet';
+
+import "./Map.css";
+import MapEventHandler from './MapEventHandler';
+
+export default function Map() {
+  const defaultPosition = new LatLng(-31.39, -57.95)
+  const defaultZoom = 12
+
+  const eventHandlerMap: LeafletEventHandlerFnMap = {
+    click: (e) => console.log(e.latlng)
+  }
+
+  return (
+    <div className="map">
+      <div>info info info</div>
+      <MapContainer 
+        center={defaultPosition} 
+        zoom={defaultZoom}
+        attributionControl={false}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          detectRetina={true}
+        />
+
+        <MapEventHandler eventMap={eventHandlerMap} />
+      </MapContainer>
+      <label className="attribution">
+        🇺🇦 <a href="https://leafletjs.com">Leaftlet</a> | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors
+      </label>
+    </div>
+  );
+};
+
