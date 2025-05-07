1331eab871c0701fcb21bbf5fda40820a281f24e
tramallo <timo.22394@hotmail.com>
2025-05-07 16:14:09 -0300

    CalendarContracts -> fix type warning

--- src/components/contracts/CalendarContracts.tsx
+++ src/components/contracts/CalendarContracts.tsx
@@ -1,30 +1,36 @@
 import { Box } from "@mui/material";
 import { DateCalendar } from "@mui/x-date-pickers";
-
-import { CustomCalendarDay } from "../CustomCalendarDay";
 import { addDays, isSameDay } from "date-fns";
 
-export function CalendarContracts() {
+import {
+  CustomCalendarDay,
+  CustomCalendarDayProps,
+} from "../CustomCalendarDay";
 
+export function CalendarContracts() {
   const highlightedDays = [
-    new Date(), 
+    new Date(),
     addDays(new Date(), 2),
     addDays(new Date(), 4),
   ];
 
   return (
     <Box padding={1}>
-      <DateCalendar 
-        showDaysOutsideCurrentMonth 
-        sx={{ border: "2px solid red"}}
+      <DateCalendar
+        showDaysOutsideCurrentMonth
+        sx={{ border: "2px solid red" }}
         slots={{ day: CustomCalendarDay }}
-        slotProps={{ 
-          day: (ownerState) => ({
-            highlightColors: highlightedDays.some((highlightedDay) => isSameDay(highlightedDay, ownerState.day))
-              ? ["green", "orange"] : undefined
-          })
+        slotProps={{
+          day: (ownerState) =>
+            ({
+              highlightColors: highlightedDays.some((highlightedDay) =>
+                isSameDay(highlightedDay, ownerState.day)
+              )
+                ? ["green", "orange"]
+                : undefined,
+            } as CustomCalendarDayProps),
         }}
       />
     </Box>
-  )
+  );
 }
