c329baa31921a188852208a7325859860e159da7
tramallo <timo.22394@hotmail.com>
2025-05-10 18:49:21 -0300

    App -> setup layout to show app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import { memo } from "react";
 
-//import MapProperties from "./components/properties/MapProperties";
 import Navigation, { NavigationSlide } from "./components/Navigation";
 import SearchPersons from "./components/persons/SearchPersons";
 import SearchRealtors from "./components/realtors/SearchRealtors";
 import SearchProperties from "./components/properties/SearchProperties";
-import Test from "./layouts/Test";
 import { Button } from "@mui/material";
 import { useAuthContext } from "./utils/helperFunctions";
 import { CalendarContracts } from "./components/contracts/CalendarContracts";
+import MapProperties from "./components/properties/MapProperties";
+import SearchContracts from "./components/contracts/SearchContracts";
 
 export function App() {
   const { endSession } = useAuthContext();
 
   const slides: NavigationSlide[] = [
-    //{ label: "Map", component: <MapProperties /> },
+    { label: "Map", component: <MapProperties /> },
     { label: "Calendar", component: <CalendarContracts /> },
     { label: "Properties", component: <SearchProperties /> },
     { label: "Persons", component: <SearchPersons /> },
     { label: "Realtors", component: <SearchRealtors /> },
-    { label: "test", component: <Test /> },
+    { label: "Contracts", component: <SearchContracts /> },
   ];
 
   return (
