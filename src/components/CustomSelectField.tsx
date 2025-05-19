619a0e65ce41677e651003bec0cfb5354ae5043e
tramallo <timo.22394@hotmail.com>
2025-05-18 23:55:41 -0300

    package.json -> add i18n dependencies || new file i18n.ts: for i18n configuration and initilization || main -> initilize i18n on startup || new file translations: declares translation languages and values || new component AppSettingsPane: displays input fields for app configuration || new component CustomSelectField: wrapper for mui select field || UserMenu -> add app settings pane | add button label translation

---
+++ src/components/CustomSelectField.tsx
@@ -0,0 +1,38 @@
+import { useId } from "react";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectProps,
+} from "@mui/material";
+
+export type CustomSelectFieldProps = SelectProps & {
+  options: Array<{ label: string; value: string }>;
+};
+
+export function CustomSelectField({
+  options,
+  ...selectProps
+}: CustomSelectFieldProps) {
+  const labelId = useId();
+
+  return (
+    <FormControl fullWidth>
+      <InputLabel id={labelId}>{selectProps.label}</InputLabel>
+      <Select
+        labelId={labelId}
+        variant="outlined"
+        {...selectProps}
+        sx={(theme) => ({ backgroundColor: theme.palette.grey[200] })}
+      >
+        <MenuItem value={undefined}>_</MenuItem>
+        {options.map(({ label, value }, index) => (
+          <MenuItem key={`select-${label}-${index}`} value={value}>
+            {label}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+}
