d062721ed6fe19f1aaea187048c5744fb42f72aa
tramallo <timothy.ramallo@gsoft.com.uy>
2025-08-25 03:33:23 -0300

    RealtorsLayout > add a onCreate callback that closes the modal

--- src/layouts/RealtorsLayout.tsx
+++ src/layouts/RealtorsLayout.tsx
@@ -51,7 +51,7 @@ export function RealtorsLayout() {
           open={createRealtorModalOpen}
           onClose={() => setCreateRealtorModalOpen(false)}
         >
-          <CreateRealtor />
+          <CreateRealtor onCreate={() => setCreateRealtorModalOpen(false)} />
         </CustomModal>
       </Stack>
     </Stack>
